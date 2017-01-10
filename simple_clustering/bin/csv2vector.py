ad847862f9761ec3a221b52b3cdbc810b53d42c1
FelipeMelis <felipe.melis@gmail.com>
2017-01-10 13:42:42 -0300

    first commit simple clustering

---
+++ simple_clustering/bin/csv2vector.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+from itertools import islice
+
+
+csv.register_dialect('excel', delimiter=';', quoting=csv.QUOTE_NONE)
+
+def open_excel_csv():
+    csv_file = open('test.csv', 'rb')
+    global reader
+    reader = csv.reader(csv_file, 'excel')
+        
+def vector_numbers():
+    for i in reader:
+        number = len(i)-1
+    print number    
+            
+def create_vector():
+    for line in islice(reader, 1, None):
+        print line[0]
+        for vector in line[1:]:
+            print vector    
+
+sys.stdout = open('file', 'w')
+print 'test'
+        
+open_excel_csv()
+vector_numbers()
+open_excel_csv()
+create_vector()
